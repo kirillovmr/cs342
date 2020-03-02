8c07a4cc05cb4d0aced2fb7e65337deff54fb532
Viktor Kirillov <mr.kirillov@icloud.com>
2020-03-02 00:01:50 -0600

    Basic money btn interface

--- ThreeCardPoker_Project2_Sp2020/src/main/java/UI/UIGameButtons.java
+++ ThreeCardPoker_Project2_Sp2020/src/main/java/UI/UIGameButtons.java
@@ -1,5 +1,6 @@
 package UI;
 
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 
@@ -18,29 +19,45 @@ public class UIGameButtons extends HBox {
     }
 
     public void createGameButtons() {
+        // Left side
         HBox leftButtonsBox = new HBox(
+                createMoneyButton(),
+                UIMisc.leftSpacer(10),
                 createButton("Play", "playButton", "btnPlay1"),
                 createButton("Fold", "foldButton", "btnFold1")
         );
         leftButtonsBox.getStyleClass().add("buttonsBox");
 
+        // Middle button
         Button dealButton = createButton("Deal", "dealButton", "btnDeal");
 
+        // Right side
         HBox rightButtonsBox = new HBox(
                 createButton("Play", "playButton", "btnPlay2"),
-                createButton("Fold", "foldButton", "btnFold2")
+                createButton("Fold", "foldButton", "btnFold2"),
+                UIMisc.leftSpacer(10),
+                createMoneyButton()
         );
         rightButtonsBox.getStyleClass().add("buttonsBox");
+
+
 
         this.getChildren().addAll(leftButtonsBox, dealButton, rightButtonsBox);
     }
 
     private Button createButton(String text, String className, String id) {
-        Button button = new Button(text);
-        button.getStyleClass().addAll("gameBtn", "shadow", className);
-        button.setId(id);
+        Button b = new Button(text);
+        b.getStyleClass().addAll("gameBtn", "shadow", className);
+        b.setId(id);
 
-        uiButtons.add(button);
-        return button;
+        uiButtons.add(b);
+        return b;
+    }
+
+    private Button createMoneyButton() {
+        Button b = new Button("MONEY: $100");
+        b.getStyleClass().addAll("gameBtn", "shadow", "moneyBtn");
+
+        return b;
     }
 }
